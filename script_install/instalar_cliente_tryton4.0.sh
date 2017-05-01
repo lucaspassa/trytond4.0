21504f766b364f1642c5a4dc6b937d184d809ec8
Luks <lucaspassa@gmail.com>
2017-05-01 19:42:24 -0300

    Update instalar_cliente_tryton4.0.sh
    Se agrega librería

--- script_install/instalar_cliente_tryton4.0.sh
+++ script_install/instalar_cliente_tryton4.0.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Instala el paquete de HG para poder clonar el código fuente de tryton
-apt-get install mercurial python-dateutil
+apt-get install mercurial python-dateutil python-chardet
 
 # se descarga el código fuente del cliente
 hg clone -b 4.0 https://hg.tryton.org/tryton
